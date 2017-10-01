7a5ef111019d9a9fc78b6aa0433ef02b5979d711
Kacper Raczy <gfw.kra@gmail.com>
2017-10-01 21:53:11 +0200

    Initial commit. Added color utility functions.

---
+++ color_util.py
@@ -0,0 +1,54 @@
+#
+# color_util.py
+#
+# Created by Kacper Raczy on 31.09.2017.
+# Copyright (c) 2017 Kacper Raczy. All rights reserved.
+#
+
+"""
+Color conversion functions working on colors from different color spaces.
+
+Current supported cs's: RGB, CMYK, GrayScale
+"""
+
+def rgbToCMYK( r, g, b):
+    """
+    Converts RGB to CMYK
+    :param r: red value (from 0.0 to 1.0)
+    :param g: green value (from 0.0 to 1.0)
+    :param b: blue value (from 0.0 to 1.0)
+    :return: tuple containing CMYK color values (from 0.0 to 1.0 each)
+    """
+    k = 1-max([r,g,b])
+    c = (1-r-k)/(1-k)
+    m = (1-g-k)/(1-k)
+    y = (1-b-k)/(1-k)
+    return c,m,y,k
+
+def cmykToRGB(c,m,y,k):
+    r = (1-c)*(1-k)
+    g = (1-m)*(1-k)
+    b = (1-y)*(1-k)
+    return r,g,b
+
+def grayToRGB(grey):
+    r = grey
+    g = grey
+    b = grey
+    return r,g,b
+
+
+def rgbToGray(r,g,b):
+    """
+    Converts RGB to GrayScale using luminosity method
+    :param r: red value (from 0.0 to 1.0)
+    :param g: green value (from 0.0 to 1.0)
+    :param b: blue value (from 0.0 to 1.0)
+    :return GreyScale value (from 0.0 to 1.0)
+    """
+    g = 0.21*r + 0.72*g + 0.07*b
+    return g
+
+def hexStringToRGB(hexString):
+    #TODO
+    return
